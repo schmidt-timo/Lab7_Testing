2fbeac3bc17992b2965a6b147a91d9c72eeedaf2
Timo Schmidt <schmidt@timo.email>
2020-06-23 14:38:46 +0200

    Absolute Code

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+
+    // Code for White-box test exercise
+    public static int absoluteValueOf (int x) {
+        if (x < -1)
+            return -x;
+        else
+            return x;
+    }
+
 }
