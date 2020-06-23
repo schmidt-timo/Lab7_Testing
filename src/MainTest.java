da6cace56c7e7820e891c26a1a54568c59609cef
MarieLencer <charlott_lencer@web.de>
2020-06-23 15:18:49 +0200

    Added Even and Odd tests

--- src/MainTest.java
+++ src/MainTest.java
@@ -4,9 +4,19 @@ class MainTest {
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+    	oddTest();
+    	evenTest();
     }
 
     @org.junit.jupiter.api.Test
-    void absoluteValueOf() {
+    void oddTest() {
+    	assertEquals(41, Main.absoluteValueOf(41));
     }
+
+    @org.junit.jupiter.api.Test
+    void evenTest() {
+    	assertEquals(6, Main.absoluteValueOf(6));
+    }
+    
+
 }
