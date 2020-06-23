bcf27dc1830e76e370090c7058fdf35e90fc05db
ThisIsKaluwa <aseriousmail@aol.com>
2020-06-23 15:13:51 +0200

    finished tests

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,12 +1,26 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Assert;
+
+
+
 class MainTest {
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+    	   Main main = new Main();
     }
 
     @org.junit.jupiter.api.Test
-    void absoluteValueOf() {
+    void zeroTest() {
+    	int zero = Main.absoluteValueOf(0);
+    	Assert.assertTrue(zero == 0);
     }
+    
+    @org.junit.jupiter.api.Test
+    void StringTest() {
+    	String testString = "test";
+    	int test = Main.absoluteValueOf(Integer.parseInt(testString));
+    }
+    
 }
