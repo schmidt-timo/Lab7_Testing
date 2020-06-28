56cb94dfc44488eab363a92a40ecca3bae33039e
Timo Schmidt <schmidt@timo.email>
2020-06-28 12:28:52 +0200

    Add TestClass

---
+++ src/TaxTimeTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaxTimeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
